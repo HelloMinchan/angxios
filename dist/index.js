cb079998091fd7858c26a19343d77e0bcfe2421c
hellominchan <wjdalscksdle@naver.com>
2021-10-04 01:13:10 +0900

    [angxios 0.0.1] babel config

---
+++ dist/index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var angxios = function angxios() {
+  var test = 1;
+  var test2 = 2;
+
+  var a = function a() {
+    console.log(a);
+  };
+
+  a();
+  console.log("hello angxios");
+  return {
+    test: test,
+    test2: test2
+  };
+};
+
+var _default = angxios;
+exports["default"] = _default;
+//# sourceMappingURL=index.js.map
--- index.js
+++
@@ -1,3 +0,0 @@
-export default function axidos() {
-  console.log("hello axidos");
-}
