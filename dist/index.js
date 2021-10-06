67c256da831b01dee83a4b6c807994421b5c68c7
hellominchan <wjdalscksdle@naver.com>
2021-10-07 01:07:04 +0900

    [angxios 0.0.8] axios wrapping

--- dist/index.js
+++ dist/index.js
@@ -9,10 +9,127 @@ var _axios = _interopRequireDefault(require("axios"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var angxios = function angxios() {
-  return _axios["default"];
-};
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var angxios = new ( /*#__PURE__*/function () {
+  function _class2() {
+    _classCallCheck(this, _class2);
+
+    _defineProperty(this, "axios", _axios["default"]);
+  }
+
+  _createClass(_class2, [{
+    key: "setBaseURL",
+    value:
+    /**
+     * @name setBaseURL
+     * @description set global axios defaults base URL
+     * @param {string} baseURL
+     */
+    function setBaseURL(baseURL) {
+      _axios["default"].defaults.baseURL = baseURL;
+    }
+    /**
+     * @name setTimeout
+     * @description set global axios defaults timeout
+     * @param {number} time
+     */
+
+  }, {
+    key: "setTimeout",
+    value: function setTimeout(time) {
+      _axios["default"].defaults.timeout = time;
+    } // request(config) {}
+    // head(url, config) {}
+
+    /**
+     * @name get
+     * @description axios get Function
+     * @param {string} url
+     * @param {?object} params
+     */
+
+  }, {
+    key: "get",
+    value: function get(url, parmas) {
+      var config = {
+        parmas: parmas ? parmas : null
+      };
+      return _axios["default"].get(url, config);
+    }
+    /**
+     * @name delete
+     * @description axios delete Function
+     * @param {string} url
+     * @param {?object} config
+     */
+
+  }, {
+    key: "delete",
+    value: function _delete(url, config) {
+      return _axios["default"].get(url, config);
+    }
+    /**
+     * @name options
+     * @description axios options Function
+     * @param {string} url
+     * @param {?object} config
+     */
+
+  }, {
+    key: "options",
+    value: function options(url, config) {
+      return _axios["default"].get(url, config);
+    }
+    /**
+     * @name post
+     * @description axios post Function
+     * @param {string} url
+     * @param {?object} body
+     * @param {?object} config
+     */
+
+  }, {
+    key: "post",
+    value: function post(url, body, config) {
+      return _axios["default"].get(url, body ? body : null, config);
+    }
+    /**
+     * @name put
+     * @description axios put Function
+     * @param {string} url
+     * @param {?object} body
+     * @param {?object} config
+     */
+
+  }, {
+    key: "put",
+    value: function put(url, body, config) {
+      return _axios["default"].get(url, body ? body : null, config);
+    }
+    /**
+     * @name patch
+     * @description axios patch Function
+     * @param {string} url
+     * @param {?object} body
+     * @param {?object} config
+     */
+
+  }, {
+    key: "patch",
+    value: function patch(url, body, config) {
+      return _axios["default"].get(url, body ? body : null, config);
+    }
+  }]);
+
+  return _class2;
+}())();
 var _default = angxios;
 exports["default"] = _default;
 //# sourceMappingURL=index.js.map
